d2ded41a1897675170f33d14a14ace142e81e6af
christopherhtol <39706071-christopherhtol@users.noreply.replit.com>
2025-02-15 22:56:57 +0000

    Agent query: Does the manager dashboard now show by default when you open the application? Are the charts properly sized and spaced?
    Redirect root path to manager dashboard
    
    Screenshot: https://storage.googleapis.com/screenshot-production-us-central1/617c2541-9870-4d8c-b31e-607d649d1043/74aac538-5ba1-48b8-9278-4840ea276a0b.jpg

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
@@ -18,7 +18,9 @@ import NotFound from "@/pages/not-found";
 function Router() {
   return (
     <Switch>
-      <Route path="/" component={Dashboard} />
+      <Route path="/">
+        <Redirect to="/manager" />
+      </Route>
       <Route path="/book/:id" component={Book} />
       {/* Manager Routes */}
       <Route path="/manager" component={ManagerHome} />
