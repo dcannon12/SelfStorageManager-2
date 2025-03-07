acb23c1909c2f339e731492084c65e1e4fc87bef
dcan <devinccannon@gmail.com>
2025-03-06 19:17:15 -0500

    030625 Update

--- vite.config.ts
+++ vite.config.ts
@@ -11,8 +11,8 @@ export default defineConfig({
   plugins: [react(), runtimeErrorOverlay(), themePlugin()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "client", "src"),
-      "@shared": path.resolve(__dirname, "shared"),
+      "@": path.resolve(__dirname, "src"),
+      "@shared": path.resolve(__dirname, "../shared"),
     },
   },
   root: path.resolve(__dirname, "client"),
